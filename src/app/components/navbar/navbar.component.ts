31b4e815e19b567b1243e2fd0510233c74a9dc65
Uszakow <pawel.uszakow@accenture.com>
2023-07-03 14:05:58 +0200

    navbar, footer, dropdown components were created

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from 'src/app/services/state.service';
+import { UserInterface } from 'src/interfaces/User.interface';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private stateService: StateService) {}
 
+  user: UserInterface | null = null;
+
+  logout = () => {
+    localStorage.removeItem('token');
+    this.stateService.updateUser();
+  };
+
+  ngOnInit(): void {
+    this.stateService.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
 }
--- src/app/components/ui/dropdown/dropdown.component.ts
+++ src/app/components/ui/dropdown/dropdown.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
 })
 export class DropdownComponent {
+  @Input() type: 'simple' = 'simple';
+  @ContentChild('titleTemplate') titleTemplate: TemplateRef<any>;
+  @ContentChild('contentTemplate') contentTemplate: TemplateRef<any>;
 
+  isOpen: boolean = false;
+
+  setIsOpen = (isOpen: boolean): void => {
+    this.isOpen = isOpen;
+  };
+
+  get dropdownClasses(): string {
+    return `dropdown ${this.type} relative`;
+  }
+  get drodpownContentClasses(): string {
+    return `${!this.isOpen ? 'none' : ''} dropdown-content`;
+  }
 }
